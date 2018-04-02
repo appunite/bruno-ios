9f9721cbe604a8dc9621a09f7647935e48d1f494
Emil <emil@appunite.com>
2018-04-03 00:24:36 +0200

    basic implementation

---
+++ Bruno/Bruno.h
@@ -0,0 +1,19 @@
+//
+//  Bruno.h
+//  Bruno
+//
+//  Created by Emil Wojtaszek on 03/04/2018.
+//  Copyright © 2018 AppUnite.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bruno.
+FOUNDATION_EXPORT double BrunoVersionNumber;
+
+//! Project version string for Bruno.
+FOUNDATION_EXPORT const unsigned char BrunoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bruno/PublicHeader.h>
+
+
